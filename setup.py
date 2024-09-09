ff442cc6d4a60191e4b7c019aaef0d06855ef09f
matteo <s307688@studenti.polito.it>
2024-09-09 15:15:14 +0000

    09.09.2024

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from distutils.core import setup
 setup(
-  name = 'YOURPACKAGENAME',         # How you named your package folder (MyLib)
-  packages = ['YOURPACKAGENAME'],   # Chose the same as "name"
+  name = 'sr620py',         # How you named your package folder (MyLib)
+  packages = ['sr620py'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
-  author = 'YOUR NAME',                   # Type in your name
-  author_email = 'your.email@domain.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  description = 'Manage a SR620 Universal Time Counter',   # Give a short description about your library
+  author = 'Matteo Tedde (Lab3841)',                   # Type in your name
+  author_email = 'teddematteo03@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/teddematteo/sr620py',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/teddematteo/sr620py/archive/refs/tags/v_01.tar.gz',    # I explain this later on
+  keywords = ['SR620', 'Time counter', 'Measurements'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'pyserial',
+          'tqdm',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
